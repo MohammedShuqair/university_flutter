4963b309ace8a0423749c445829c0fb2edeadd78
mohammed_shuqair <eng.mohammed.shuqair@gmail.com>
2023-10-13 20:17:48 +0300

    fix s latter

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:university_app/prefs/shared_prefs_controller.dart';
 
